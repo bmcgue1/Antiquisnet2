aa2587bdc6ab6dd38121533957f430a705e9e0c3
mike2flee <Micjj78@gmail.com>
2019-01-28 18:18:24 -0500

    progressed sign up page and adding skeleton for industry check list

--- src/js/hide.js
+++ src/js/hide.js
@@ -1,4 +1,6 @@
-function creds() {
+//Reister & Login form swtich
+var btn = document.getElementById("register_login");
+btn.onclick = function() {
   var login = document.getElementById("login");
   var register = document.getElementById("signup");
   var cocunt;
@@ -14,14 +16,48 @@ function creds() {
       login.style.display = "block";
       register.style.display = "none";
       count = 2;
-      document.getElementById("pills-home-tab").innerHTML = "Register";
+      document.getElementById("register_login").innerHTML = "Register";
       break;
 
     case 2:
       login.style.display = "none";
       register.style.display = "block";
       count = 1;
-      document.getElementById("pills-home-tab").innerHTML = "Login";
+      document.getElementById("register_login").innerHTML = "Login";
       break;
   }
-}
+};
+
+// var industry = document.getElementById("industry");
+
+// industry.onclick = function() {
+//   var job = document.getElementById("boxx");
+//   var form = document.getElementById("signup").blur;
+//   job.style.display = "contents";
+//   console.log("job on click ");
+// };
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
